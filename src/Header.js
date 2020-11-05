43c1858259dcf813884151b7086da71c7ef734bb
AdityaSatyam <aditya.satyam@infosys.com>
2020-11-06 02:36:09 +0530

    newpush

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Header.css';
-// import SearchIcon from "@material-ui/icons"
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket'
 function Header() {
